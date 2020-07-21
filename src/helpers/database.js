0862f543c72c63b07bfa22129f9351ea3300136e
Jorge Bautista <jorgebautista@gmail.com>
2020-07-20 23:43:22 -0400

    migrating to gitlab

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,30 +1,28 @@
-
 import firebase from 'firebase';
-
+//THIS IS A PERSONAL PRIVATE PROJECT. THIS SHOULD NEVER BE DONE IN PRODUCTION
+//ENVIRONMENT VARIABLES SHOULD BE USED.
 const production = {
-  apiKey: "***REMOVED***",
-  authDomain: "***REMOVED***",
-  databaseURL: "***REMOVED***",
-  projectId: "***REMOVED***",
-  storageBucket: "***REMOVED***.appspot.com",
-  messagingSenderId: "***REMOVED***"
+	apiKey: '***REMOVED***',
+	authDomain: '***REMOVED***',
+	databaseURL: '***REMOVED***',
+	projectId: '***REMOVED***',
+	storageBucket: '***REMOVED***.appspot.com',
+	messagingSenderId: '***REMOVED***'
 };
 
 const development = {
-  apiKey: "***REMOVED***",
-  authDomain: "***REMOVED***",
-  databaseURL: "***REMOVED***",
-  projectId: "***REMOVED***",
-  storageBucket: "***REMOVED***.appspot.com",
-  messagingSenderId: "***REMOVED***"
+	apiKey: '***REMOVED***',
+	authDomain: '***REMOVED***',
+	databaseURL: '***REMOVED***',
+	projectId: '***REMOVED***',
+	storageBucket: '***REMOVED***.appspot.com',
+	messagingSenderId: '***REMOVED***'
 };
 
-
-// const firebaseApp = firebase.initializeApp(production);
-const firebaseApp = firebase.initializeApp(development);
+const firebaseApp = firebase.initializeApp(production);
+// const firebaseApp = firebase.initializeApp(development);
 const database = firebase.database();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 const facebookProvider = new firebase.auth.FacebookAuthProvider();
 const firebaseAuth = firebase.auth;
-export { firebaseApp, googleProvider,facebookProvider, firebaseAuth, database as default }
-
+export { firebaseApp, googleProvider, facebookProvider, firebaseAuth, database as default };
